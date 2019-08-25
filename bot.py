95ee4bbaae1c6f309577d311e920f38a73a11a82
CUDDLER <53143464+CUDDLER@users.noreply.github.com>
2019-08-25 14:35:17 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,14 +4,19 @@ import telebot
 
 bot=telebot.TeleBot(config.token)
 
-@bot.message_handler(commands=['start']) #Старт бота
+@bot.message_handler(commands=['start', 'help']) #Старт бота
 def start_handler(message):
-    keyboard = types.InlineKeyboardMarkup()
-    callback_button = types.InlineKeyboardButton(text="Начать", callback_data="test")
-    keyboard.add(callback_button)
-    bot.send_message(message.chat.id, 'Хай', reply_markup=keyboard)
+   if message.text == 'Привет' or '/start':
+       bot.send_message(message.chat.id, 'Хай')
+   elif message.text == 'Помощь' or '/help':
+       bot.send_message(message.chat.id, 'Чем тебе помочь?')
+    # keyboard = types.InlineKeyboardMarkup()
+    # callback_button = types.InlineKeyboardButton(text="Начать", callback_data="test")
+    # keyboard.add(callback_button)
+    # bot.send_message(message.chat.id, 'Хай', reply_markup=keyboard)
 #Привет, '+ str(user.first_name) + ' это бот-конфигуратор OmenBoyzCustom /n'
 #                                                                        'Я помогу тебе сделать именно тот дизайн, который ты хочешь! Жми "Начать"
+
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
     bot.send_message(message.chat.id, message.text)
