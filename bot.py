34c4ca667835848ac9dbb37a2d1454b45d3e573d
CUDDLER <53143464+CUDDLER@users.noreply.github.com>
2019-08-25 14:29:34 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,9 +9,9 @@ def start_handler(message):
     keyboard = types.InlineKeyboardMarkup()
     callback_button = types.InlineKeyboardButton(text="Начать", callback_data="test")
     keyboard.add(callback_button)
-    bot.send_message(message.chat.id, 'Привет, '+ str(user.first_name) + ' это бот-конфигуратор OmenBoyzCustom /n'
-                                                                         'Я помогу тебе сделать именно тот дизайн, который ты хочешь! Жми "Начать"')
-
+    bot.send_message(message.chat.id, 'Хай', reply_markup=keyboard)
+#Привет, '+ str(user.first_name) + ' это бот-конфигуратор OmenBoyzCustom /n'
+#                                                                        'Я помогу тебе сделать именно тот дизайн, который ты хочешь! Жми "Начать"
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
     bot.send_message(message.chat.id, message.text)
