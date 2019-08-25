5856b8ffdc03cd1dc3500500dc0cd833342fbfdd
CUDDLER <53143464+CUDDLER@users.noreply.github.com>
2019-08-25 14:24:44 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,16 @@
 import config
 import telebot
 
+
 bot=telebot.TeleBot(config.token)
+
+@bot.message_handler(commands=['start']) #Старт бота
+def start_handler(message):
+    keyboard = types.InlineKeyboardMarkup()
+    callback_button = types.InlineKeyboardButton(text="Начать", callback_data="test")
+    keyboard.add(callback_button)
+    bot.send_message(message.chat.id, 'Привет, '+ str(user.first_name) + ' это бот-конфигуратор OmenBoyzCustom /n'
+                                                                         'Я помогу тебе сделать именно тот дизайн, который ты хочешь! Жми "Начать"')
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
