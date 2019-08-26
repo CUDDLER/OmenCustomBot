ee74c1225e6d7bac3d3ee00ae0cde36fd7d64c62
CUDDLER <53143464+CUDDLER@users.noreply.github.com>
2019-08-26 19:10:41 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,21 +5,14 @@ bot=telebot.TeleBot(config.token)
 
 @bot.message_handler(content_types=["text"]) #Старт бота
 def command_handler(message):
-    markdown = """
-        *bold text*
-        _italic text_
-        [text](URL)
-        """
-    ret_msg = telebot.send_message(CHAT_ID, markdown, parse_mode="Markdown")
-    assert ret_msg.message_id
-    
+
     if message.text == 'Привет' or '/start':
         keyboard_start = telebot.types.InlineKeyboardMarkup()
         callback_button = telebot.types.InlineKeyboardButton(text="Начать", callback_data="test")
         keyboard_start.add(callback_button)
-        bot.send_message(message.chat.id, "Привет, " + str(message.chat.first_name) + ", это бот-конфигуратор *OmenBoyzCustom* \n"
+        bot.send_message(message.chat.id, "Привет, " + str(message.chat.first_name) + ", это бот-конфигуратор OmenBoyzCustom \n"
                                           "Я помогу тебе сделать именно тот дизайн, который ты хочешь!\n "
-                                          "Жми *Начать*", parse_mode="Markdown")
+                                          "Жми Начать")
     elif message.text == 'Помощь' or '/help':
         bot.send_message(message.chat.id, "Чем *тебе* помочь?", parse_mode="Markdown")
 
