d1f723c4e335dda110338c9caeffb8ec4ae7249b
CUDDLER <53143464+CUDDLER@users.noreply.github.com>
2019-08-26 19:13:29 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,16 +5,15 @@ bot=telebot.TeleBot(config.token)
 
 @bot.message_handler(content_types=["text"]) #Старт бота
 def command_handler(message):
-
     if message.text == 'Привет' or '/start':
         keyboard_start = telebot.types.InlineKeyboardMarkup()
         callback_button = telebot.types.InlineKeyboardButton(text="Начать", callback_data="test")
         keyboard_start.add(callback_button)
-        bot.send_message(message.chat.id, "Привет, " + str(message.chat.first_name) + ", это бот-конфигуратор OmenBoyzCustom \n"
-                                          "Я помогу тебе сделать именно тот дизайн, который ты хочешь!\n "
-                                          "Жми Начать")
+        bot.send_message(message.chat.id, "Привет, " + str(message.chat.first_name) + ", это бот-конфигуратор *OmenBoyzCustom* \n"
+                                         "Я помогу тебе сделать именно тот дизайн, который ты хочешь!\n "
+                                         "Жми *Начать*", reply_markup=keyboard_start)
     elif message.text == 'Помощь' or '/help':
-        bot.send_message(message.chat.id, "Чем *тебе* помочь?", parse_mode="Markdown")
+        bot.send_message(message.chat.id, 'Чем тебе помочь?')
 
 def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
     bot.send_message(message.chat.id, message.text)
