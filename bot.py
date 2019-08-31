dcbefd88967466ae26a74fe8f7ff5e42475288d0
CUDDLER <53143464+CUDDLER@users.noreply.github.com>
2019-08-31 13:04:44 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,9 +16,8 @@ def command_handler(message):
 def command_handler(message):
     if 'Помощь' or '/help' in message.text:
         bot.send_message(message.chat.id, 'Чем тебе помочь?')
-
-def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
-    bot.send_message(message.chat.id, message.text)
+    else # Название функции не играет никакой роли, в принципе
+        bot.send_message(message.chat.id, message.text)
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
