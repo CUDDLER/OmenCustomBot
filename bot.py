8a291fadd5589110623c83de67d0f06a409e5e61
CUDDLER <53143464+CUDDLER@users.noreply.github.com>
2019-08-31 12:53:23 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,18 +3,19 @@ import telebot
 
 bot=telebot.TeleBot(config.token)
 
+@bot.message_handler(commands=['start'])
+def command_handler(message: Message):
+    keyboard_start = telebot.types.InlineKeyboardMarkup()
+    callback_button = telebot.types.InlineKeyboardButton(text="Начать", callback_data="test")
+    keyboard_start.add(callback_button)
+    bot.reply_to(message, 'Привет, ' + str(message.chat.first_name) + ', это бот-конфигуратор OmenBoyzCustom \n'
+                                                                 'Я помогу тебе сделать именно тот дизайн, который ты хочешь!\n '
+                                                                 'Жми "Начать"')
+
 @bot.message_handler(content_types=["text"]) #Старт бота
 def command_handler(message):
-    if telebot.message.text == 'Привет' or '/start':
-        keyboard_start = telebot.types.InlineKeyboardMarkup()
-        callback_button = telebot.types.InlineKeyboardButton(text="Начать", callback_data="test")
-        keyboard_start.add(callback_button)
-        bot.send_message(message.chat.id, "Привет, " + str(message.chat.first_name) + ", это бот-конфигуратор *OmenBoyzCustom* \n"
-                                         "Я помогу тебе сделать именно тот дизайн, который ты хочешь!\n "
-                                         "Жми *Начать*", reply_markup=keyboard_start)
-    elif telebot.message.text == 'Помощь' or '/help':
+    if 'Помощь' or '/help' in message.text:
         bot.send_message(message.chat.id, 'Чем тебе помочь?')
-
 def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
     bot.send_message(message.chat.id, message.text)
 
