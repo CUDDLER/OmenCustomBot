bc9c495c34b55e0cbed87de89cc9cea02728307a
CUDDLER <53143464+CUDDLER@users.noreply.github.com>
2019-09-04 18:38:59 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,15 +15,15 @@ def command_handler(message):
 
 @bot.message_handler(content_types=["text"]) #Старт бота для сообщений
 def command_handler(message):
-    try:
-        if message.text == "/help":
-            bot.reply_to(message, 'Чем тебе помочь?')
-            return
-        elif message.text == "/config":
-            bot.reply_to(message, 'Начала конфигуратора')
-            return
-    except Exception as error:
-        print("Excepcion in forward handler. Info: {}".format(error))
+    #try:
+    if message.text == "/help":
+        bot.reply_to(message, 'Чем тебе помочь?')
+        return
+    elif message.text == "/config":
+        bot.reply_to(message, 'Начала конфигуратора')
+        return
+   # except Exception as error:
+   #     print("Excepcion in forward handler. Info: {}".format(error))
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
