13b54df1e7c0662cc6d6b933e345ef04669e7def
CUDDLER <53143464+CUDDLER@users.noreply.github.com>
2019-09-04 22:01:45 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -24,12 +24,14 @@ def command_handler(message):
 
 @bot.message_handler(commands=['sources'])
 def command_handler(message):
-    keyboard_url = telebot.types.InlineKeyboardMarkup() # resize_keyboard="False"
+
     callback_button_vk = telebot.types.InlineKeyboardButton(url="https://vk.com/omenboyzapparel", text="ВК")
     callback_button_inst = telebot.types.InlineKeyboardButton(url="https://instagram.com/omenboyz", text="Inst")
     callback_button_instcustom = telebot.types.InlineKeyboardButton(url="https://vk.com/omenboyzcustoms", text="ВК Custom")
     callback_button_vkcustom = telebot.types.InlineKeyboardButton(url="https://instagram.com/omenboyzcustoms", text="Inst Custom")
-    keyboard_url.add(callback_button_vk, callback_button_vkcustom, callback_button_instcustom, callback_button_inst)
+    keyboard_url_add=[[callback_button_vk, callback_button_vkcustom],
+                      [callback_button_instcustom, callback_button_inst]]
+    keyboard_url = telebot.types.InlineKeyboardMarkup(keyboard_url_add)  # resize_keyboard="False"
     bot.send_message(message.from_user.id, 'Ниже представлены наши сети, тебя там ждут :)', reply_markup = keyboard_url)
 
 @bot.message_handler(content_types=["text"]) #Старт бота для сообщений
