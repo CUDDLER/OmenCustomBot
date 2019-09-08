b72cc2f6b9ad83a1d31f9e9d72976f7b7797b19e
CUDDLER <bagldya@gmail.com>
2019-09-08 15:42:23 +0300

    синтаксис params

--- params.py
+++ params.py
@@ -1,7 +1,2 @@
-start_message='Привет, ' + str(message.chat.first_name) + ', это бот-конфигуратор OmenBoyzCustom \n '+
-                                                          'Я помогу тебе сделать именно тот дизайн, который ты хочешь!\n'+
-                                                          ' Жми "Начать"'
-help_message='Доступные команды: \n'+
-             '/start — Запуск бота \n'+
-             '/help — Помощь \n'+
-             '/sources — Ресурсы OmenBoyz \n'
+start_message='Привет, ' + str(message.chat.first_name) + ', это бот-конфигуратор OmenBoyzCustom \n Я помогу тебе сделать именно тот дизайн, который ты хочешь!\n Жми "Начать"'
+help_message='Доступные команды: \n /start — Запуск бота \n /help — Помощь \n /sources — Ресурсы OmenBoyz \n'
