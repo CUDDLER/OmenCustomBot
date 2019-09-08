10de2abffbcde31d33c955447703af5b947b6c65
CUDDLER <bagldya@gmail.com>
2019-09-08 15:58:33 +0300

    попытка импорта нужного типа уже из библеотеки телеги

--- params.py
+++ params.py
@@ -1,3 +1,3 @@
-from telebot import types
+from telegram import message
 start_message='Привет, ' + str(message.chat.first_name) + ', это бот-конфигуратор OmenBoyzCustom \n Я помогу тебе сделать именно тот дизайн, который ты хочешь!\n Жми "Начать"'
 help_message='Доступные команды: \n /start — Запуск бота \n /help — Помощь \n /sources — Ресурсы OmenBoyz \n'
