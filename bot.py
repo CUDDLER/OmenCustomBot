40d995e9bb7fa1011ac991272cf907eb3c2b0087
CUDDLER <53143464+CUDDLER@users.noreply.github.com>
2019-10-24 22:22:03 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,45 +3,53 @@ from os import environ
 from params import message_all
 import params
 
-bot =  environ['token']
+bot = environ['token']
+
 
 @bot.message_handler(commands=['start'])
-def command_handler(message):
+def start_handler(message):
     keyboard_start = telebot.types.InlineKeyboardMarkup()
-    callback_button = telebot.types.InlineKeyboardButton(text="Начать", callback_data="start_config")
+    callback_button = telebot.types.InlineKeyboardButton(text='Начать', callback_data="start_config")
     keyboard_start.add(callback_button)
-    user=message.chat.first_name
-    start_mes=message_all(user)
+    user = message.chat.first_name
+    start_mes = message_all(user)
     bot.send_message(message.from_user.id, start_mes)
-    
+
+
 @bot.message_handler(commands=['help'])
-def command_handler(message):
+def help_handler(message):
     keyboard_start = telebot.types.InlineKeyboardMarkup()
-    callback_button = telebot.types.InlineKeyboardButton(text="Начать", callback_data="")
+    callback_button = telebot.types.InlineKeyboardButton(text='Начать', callback_data="")
     keyboard_start.add(callback_button)
     bot.send_message(message.from_user.id, params.help_message)
 
+
 @bot.message_handler(commands=['sources'])
-def command_handler(message):
+def sources_handler(message):
     keyboard_url = telebot.types.InlineKeyboardMarkup()  # resize_keyboard="False"
-    callback_button_vk = telebot.types.InlineKeyboardButton(url="https://vk.com/omenboyzapparel", text="ВК")
-    callback_button_inst = telebot.types.InlineKeyboardButton(url="https://instagram.com/omenboyz", text="Inst")
-    callback_button_instcustom = telebot.types.InlineKeyboardButton(url="https://vk.com/omenboyzcustoms", text="ВК Custom")
-    callback_button_vkcustom = telebot.types.InlineKeyboardButton(url="https://instagram.com/omenboyzcustoms", text="Inst Custom")
+    callback_button_vk = telebot.types.InlineKeyboardButton(url="https://vk.com/omenboyzapparel", text='ВК')
+    callback_button_inst = telebot.types.InlineKeyboardButton(url="https://instagram.com/omenboyz", text='Inst')
+    callback_button_instcustom = telebot.types.InlineKeyboardButton(url="https://vk.com/omenboyzcustoms",
+                                                                    text='ВК Custom')
+    callback_button_vkcustom = telebot.types.InlineKeyboardButton(url="https://instagram.com/omenboyzcustoms",
+                                                                  text='Inst Custom')
     keyboard_url.add(callback_button_vk, callback_button_vkcustom,
                      callback_button_instcustom, callback_button_inst)
     print(message.chat.first_name)
-    bot.send_message(message.from_user.id, 'Ниже представлены наши сети, тебя там ждут :)', reply_markup = keyboard_url)
+    bot.send_message(message.from_user.id, 'Ниже представлены наши сети, тебя там ждут :)', reply_markup=keyboard_url)
 
-@bot.message_handler(content_types=["text"]) #Старт бота для сообщений
-def command_handler(message):
-    #try:
+
+@bot.message_handler(content_types=['text'])  # Старт бота для сообщений
+def text_handler(message):
+    # try:
     #    return
     if message.text == "/config":
         bot.reply_to(message, 'Начала конфигуратора')
         return
-   # except Exception as error:
-   #     print("Excepcion in forward handler. Info: {}".format(error))
+
+
+# except Exception as error:
+#     print("Excepcion in forward handler. Info: {}".format(error))
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
